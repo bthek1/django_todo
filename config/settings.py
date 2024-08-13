f97d961c3c29c0e743a5edf493ad175ad9f151bc
Benedict Thekkel <bthekkel1@gmail.com>
2024-08-13 17:28:53 +1000

    asfd

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n^-b*@3s=8fbhtf^=n^w^dkcjknogvr5!pwujoa4xam@z*adhx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [140.238.195.138, localhost,]
+ALLOWED_HOSTS = ['140.238.195.138', 'localhost', '127.0.0.1']
 
 
 # Application definition
