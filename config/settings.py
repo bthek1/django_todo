5b62164634f75a5c3915d6075938f542cd390619
Benedict Thekkel <bthekkel1@gmail.com>
2024-08-13 17:24:47 +1000

    update

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+# Add this setting
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 STATIC_URL = 'static/'
 
